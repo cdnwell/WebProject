2bf2996ce268f6bae3f8310bfc664008f3bc913b
Sang Hyeok Hwang <cdnwellhk@gmail.com>
2022-06-05 12:10:43 +0900

    commit 6.5

---
+++ pro07/src/main/java/sec01/ex02/MemberServlet.java
@@ -0,0 +1,52 @@
+package sec01.ex02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+import java.util.List;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/member")
+public class MemberServlet extends HttpServlet {
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init 메서드 호출");
+	}
+
+	public void destroy() {
+		System.out.println("destroy 메서드 호출");
+	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		MemberDAO dao = new MemberDAO();
+		List<MemberVO> list = dao.listMembers();
+		
+		out.print("<html><body>");
+		out.print("<table border=1><tr align='center' bgcolor='lightgreen'>");
+		out.print("<td>아이디</td>");
+		out.print("<td>비밀번호</td>");
+		out.print("<td>이름</td>");
+		out.print("<td>이메일</td>");
+		out.print("<td>가입일</td>");
+		out.print("</tr>");
+		
+		for(int i=0;i<list.size();i++) {
+			MemberVO memberVO = list.get(i);
+			String id = memberVO.getId();
+			String pwd = memberVO.getPwd();
+			String name = memberVO.getName();
+			String email = memberVO.getEmail();
+			Date joinDate = memberVO.getJoinDate();
+			out.print("<tr><td>"+id+"</td><td>" + pwd +"</td><td>" + name +"</td><td>" 
+			+ email + "</td><td>" + joinDate +"</td></tr>");
+		}
+		out.print("</table></body></html>");
+	}
+
+}
